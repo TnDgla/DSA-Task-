eae6c7c8ec86bda5fcd2eb15d2a1986d6280a50a
Kush Varshney <122551967+Kush-Varshney@users.noreply.github.com>
2024-10-20 02:16:37 +0530

    Time: 9 ms (94.64%), Space: 47.9 MB (86.45%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count =0;
+        int i=0,j=0;
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
+                count++;
+                i++;
+                j++;
+            } else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
