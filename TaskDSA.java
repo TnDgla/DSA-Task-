f767460e1ca0a1338fc8f4874ea07885606a9398
rohitsingh290320052 <rohit.singh2_cs22@gla.ac.in>
2024-10-19 14:59:00 +0530

    minimum column value

--- TaskDSA.java
+++ TaskDSA.java
@@ -48,4 +48,19 @@ public class assigncookies {
         }
     }
 
+    class minimum column value{
+        class Solution {
+            public int getCommon(int[] nums1, int[] nums2) {
+                int i = 0, j = 0;
+                while(i < nums1.length && j < nums2.length ){
+                    if(nums1[i] == nums2[j]) return nums1[i];
+                    if(nums1[i] > nums2[j]) j++;
+                    else i++;
+                }
+                return -1;
+
+            }
+        }
+    }
+
 
