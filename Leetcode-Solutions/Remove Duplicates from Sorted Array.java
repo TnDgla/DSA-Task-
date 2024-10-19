08f04fa54be8e0213e49e7091d930d384c87555a
Deepti Agarwal <deepti126agarwal@gmail.com>
2024-10-19 08:45:44 +0530

    Added files

---
+++ Leetcode-Solutions/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=0; j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
