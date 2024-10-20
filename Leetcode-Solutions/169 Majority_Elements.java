2fedc535753ed5e2464d763f425576bc9f0f2a5f
Ekta Agrawal <119687476+EktaAgrawal08@users.noreply.github.com>
2024-10-20 23:41:25 +0530

    169 Majority Elements.java

--- Leetcode-Solutions/169 Majority_Elements.java
+++ Leetcode-Solutions/169 Majority_Elements.java
@@ -1,20 +1,13 @@
 class Solution {
-    public static int count(int[] nums, int ele){
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==ele)
-            c++;
-        }
-        return c;
-    }
-    public int majorityElement(int[] nums) {
-        int n=nums.length;
-        int i=0;
-        for(i=0;i<nums.length;i++){
-            if (count(nums,nums[i])>(n/2)){
-                break;
+    public boolean check(int[] arr) {
+        int drop = 0;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i - 1] > arr[i]){
+                drop++;
             }
         }
-        return nums[i];
+
+        if(arr[0] < arr[arr.length - 1]) drop++;
+        return drop <= 1;
     }
 }
