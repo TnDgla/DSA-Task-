740adff1c1ca6c4ebfa4fdc3149b61c2e9dae7ce
Kush Varshney <122551967+Kush-Varshney@users.noreply.github.com>
2024-10-20 12:57:09 +0530

    Time: 0 ms (100%), Space: 52 MB (37.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,33 @@
+import java.util.AbstractList;
 class Solution {
+    private List<List<Integer>> res;
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        Set<List<Integer>> ans=new HashSet<>();
-        for(int i = 0; i < nums.length-2; i++){
-            int p1 = i+1;
-            int p2 = nums.length-1;
-            while(p1 < p2){
-                int sum = nums[i]+nums[p1]+nums[p2];
-                if(sum == 0){
-                    ArrayList<Integer> sp = new ArrayList<>();
-                    sp.add(nums[i]);
-                    sp.add(nums[p1]);
-                    sp.add(nums[p2]);
-                    
-                    ans.add(sp);
-                    p1++;
-                }
-                else if(sum < 0){
-                    p1++;
-                }
-                else{
-                    p2--;
-                }
+        return new AbstractList<List<Integer>>() {
+            public List<Integer> get(int index) {
+                init();
+                return res.get(index);
             }
-        }
-        return new ArrayList<>(ans);
+            public int size() {
+                init();
+                return res.size();
+            }
+            private void init() {
+                if (res != null) return;
+                Arrays.sort(nums);
+                int l, r, sum;
+                Set<List<Integer>> tempRes = new HashSet<>();
+                for(int i = 0; i < nums.length - 2; ++i) {
+                    l = i + 1;
+                    r = nums.length - 1;
+                    while(l < r) {
+                        sum = nums[i] + nums[l] + nums[r];
+                        if (sum == 0) tempRes.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        if (sum < 0) ++l; else --r;
+                    }
+                }
+                res = new ArrayList<List<Integer>>(tempRes);
+            }
+            
+        };
     }
 }
