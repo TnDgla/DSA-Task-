665abfdea044260e44568768025b2e2e602c98fe
rohitsingh290320052 <rohit.singh2_cs22@gla.ac.in>
2024-10-20 10:26:11 +0530

    divide array into subarray with max difference

--- TaskDSA.java
+++ TaskDSA.java
@@ -174,3 +174,20 @@ class number_of_Substring_only1{
     }
 
 }
+class subarray_with_max_difference{
+
+
+    class Solution {
+        public int[][] divideArray(int[] nums, int k) {
+            Arrays.sort(nums);
+            int [][] ans = new int[nums.length/3][3];
+            for(int i = 2; i < nums.length; i += 3){
+                if(nums[i] - nums[i-2] > k) return new int[0][];
+                ans[i/3] = new int[]{nums[i-2], nums[i-1],nums[i]};
+            }
+            return ans;
+        }
+    }
+
+
+}
