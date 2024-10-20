bd7b19cc019ff75f922ac4c205d80a7ab868ac05
sakshamsaxena <saxenasaksham09@gmail.com>
2024-10-20 11:54:05 +0530

    added Easy_problems

---
+++ Easy_problems/assigncookies.java
@@ -0,0 +1,41 @@
+
+// 455. ASSign Cookies
+// https://leetcode.com/problems/assign-cookies/
+import java.util.*;
+
+public class assigncookies {
+
+    public static int findContentChildren(int[] greedFactors, int[] cookieSizes) {
+        Arrays.sort(greedFactors);
+        Arrays.sort(cookieSizes);
+        int i = 0, j = 0;
+        int count = 0;
+
+        while (i < greedFactors.length && j < cookieSizes.length) {
+            if (cookieSizes[j] >= greedFactors[i]) {
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] greedFactors = new int[n];
+        int[] cookieSizes = new int[k];
+
+        for (int i = 0; i < n; i++) {
+            greedFactors[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < k; i++) {
+            cookieSizes[i] = sc.nextInt();
+        }
+        System.out.println(findContentChildren(greedFactors, cookieSizes));
+    }
+}
