ef6ab1083b04cac023fc3945c4c0adbbdd3c8f26
Durgesh Bahadur Singh <115790234+Georgiandurgesh@users.noreply.github.com>
2024-10-20 21:42:19 +0530

    Time: 16 ms (24.93%), Space: 44.8 MB (16.07%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+       List<String> list = new ArrayList<>();
+        Map<Character, Integer> map = freqMap(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            Map<Character, Integer> currentMap = freqMap(words[i]);
+
+            for (char c : map.keySet()) {
+                if (currentMap.containsKey(c)) {
+                    map.put(c, Math.min(map.get(c), currentMap.get(c)));
+                } else {
+                    map.put(c, 0);
+                }
+            }
+        }
+
+        for (char c : map.keySet()) {
+            int count = map.get(c);
+            for (int i = 0; i < count; i++) {
+                list.add(String.valueOf(c));
+            }
+        }
+        return list;
+    }
+
+    Map<Character, Integer> freqMap(String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            map.put(word.charAt(i), map.getOrDefault(word.charAt(i), 0) + 1);
+        }
+        return map; 
+    }
+}
