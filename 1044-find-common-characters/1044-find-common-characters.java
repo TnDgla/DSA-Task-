ce3da4723c0100886305dde9e62cfd980374c1eb
Kush Varshney <122551967+Kush-Varshney@users.noreply.github.com>
2024-10-20 11:42:14 +0530

    Time: 3 ms (91.84%), Space: 42.8 MB (81.65%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.java
+++ 1044-find-common-characters/1044-find-common-characters.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        Map<Character, Integer> hmap = new HashMap<>();
-        for (char c : words[0].toCharArray()) {
-            hmap.put(c, hmap.getOrDefault(c, 0) + 1);
+        int charCount[] = new int[26];
+        for(char ch: words[0].toCharArray()){
+            charCount[ch-'a']++;
         }
 
-        for (int i = 1; i < words.length; i++) {
-            Map<Character, Integer> freq_word = new HashMap<>();
-            for (char c : words[i].toCharArray()) {
-                freq_word.put(c, freq_word.getOrDefault(c, 0) + 1);
+        for(int i=1; i<words.length; i++){
+            int current[] = new int[26];
+            for(char c2: words[i].toCharArray()){
+                current[c2-'a']++;
             }
 
-            for (char key : hmap.keySet()) {
-                hmap.put(key, Math.min(hmap.getOrDefault(key, 0), freq_word.getOrDefault(key, 0)));
+            for(int j=0; j<26; j++){
+                charCount[j]=Math.min(charCount[j],current[j]);
+            }
+        }
+        List<String> result = new ArrayList<>();
+        for(int i=0; i<26; i++){
+            for(int j=0; j<charCount[i]; j++){
+                result.add(String.valueOf((char)(i+'a')));
             }
         }
 
-        List<String> list = new ArrayList<>();
-        for (char key : hmap.keySet()) {
-            for (int i = 0; i < hmap.get(key); i++) {
-                list.add(String.valueOf(key));
-            }
-        }
-
-        return list;
+        return result;
     }
 }
