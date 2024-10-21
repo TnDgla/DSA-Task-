a53c4315b1876f03fc5fc21b767788ed56655053
iarpitsaxena <the.arpit.saxena@gmail.com>
2024-10-21 22:00:29 +0530

    Week 1 Questions done

---
+++ assignCookies.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int m=g.length;
+        int n=s.length;
+       int i=0,j=0;
+       while(i<m && j<n){
+                if(g[i]<=s[j]){
+                    i++;
+                    j++;
+                }
+                else{
+            j++;
+        }
+       }
+        return i;
+    }
+}
