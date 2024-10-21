4e13166d677afa6382b89ce593acc97c613629f7
rohitsingh290320052 <rohit.singh2_cs22@gla.ac.in>
2024-10-21 11:25:24 +0530

    3sum

--- TaskDSA.java
+++ TaskDSA.java
@@ -224,3 +224,41 @@ class find_common_characters{
         }
     }
 }
+class 3sum{
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+
+                if(sum==0){
+                    List<Integer> temp=Arrays.asList(nums[i],nums[left],nums[right]);
+                    ans.add(temp);
+                    left++;
+                    right--;
+                    while(left<right && nums[left]==nums[left-1])
+                        left++;
+                    while(left<right && nums[right]==nums[right+1])
+                        right--;
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+
+            }
+        }
+        return ans;
+    }
+}
+
+        }
