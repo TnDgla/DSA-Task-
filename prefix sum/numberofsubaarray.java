0fde5aaf5ea04b23d171ffc3d385714d0bff7537
sakshamsaxena <saxenasaksham09@gmail.com>
2024-10-21 22:20:01 +0530

    added prefix sum

---
+++ prefix sum/numberofsubaarray.java
@@ -0,0 +1,19 @@
+package prefix sum;
+// 1248. Count Number of Nice Subarrays
+
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] cnt = new int[n+1];
+        cnt[0] = 1;
+        int ans=0 , t=0;
+        for(int v : nums){
+            t+=v&1;
+            if(t-k>=0){
+                ans+=cnt[t-k];
+            }
+            cnt[t]++;
+        }
+            return ans;
+    }
+}
