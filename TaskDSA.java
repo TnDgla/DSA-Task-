84086364d98f4606395a59ec06018157bfb7eb22
rohitsingh290320052 <rohit.singh2_cs22@gla.ac.in>
2024-10-21 22:33:07 +0530

    Longest Mountain Array

--- TaskDSA.java
+++ TaskDSA.java
@@ -295,5 +295,36 @@ class Solution {
         return closestSum;
     }
 }
+}
 
+class Longest_Mountain_arry{
+    class Solution {
+        public int longestMountain(int[] arr) {
+            if (arr.length < 3) {
+                return 0;
+            }
+
+            int maxLength = 0;
+            int n = arr.length;
+
+            for (int i = 1; i < n - 1; i++) {
+                if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                    int left = i - 1;
+                    while (left > 0 && arr[left] > arr[left - 1]) {
+                        left--;
+                    }
+                    int right = i + 1;
+                    while (right < n - 1 && arr[right] > arr[right + 1]) {
+                        right++;
+                    }
+                    int currentLength = right - left + 1;
+                    maxLength = Math.max(maxLength, currentLength);
+                    i = right;
+                }
+            }
+
+            return maxLength;
         }
+    }
+
+}
