a9424e4fe00e0baf45ae55a33da73660a3a32a06
Suyash Tripathi <157511982+suyashtripathi333@users.noreply.github.com>
2024-10-21 13:16:46 +0530

    Time: 1 ms (98.59%), Space: 41.6 MB (28.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+     int i = -1;
+        char[] stack = new char[s.length()];
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                stack[++i] = ch;
+            else {
+                if (i >= 0
+                    && ((stack[i] == '(' && ch == ')')
+                        || (stack[i] == '{' && ch == '}')
+                        || (stack[i] == '[' && ch == ']')))
+                    i--;
+                else
+                    return false;
+            }
+        }
+        return i == -1;
+}
+}
