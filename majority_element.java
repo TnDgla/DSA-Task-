f2d5bbe2b12e2f09ad5a2a19d74b1da162f36746
DHEERAJ VASHISHTHA <dheeraj.vashishtha_cs22@gla.ac.in>
2024-10-22 14:12:09 +0530

    Add files via upload

---
+++ majority_element.java
@@ -0,0 +1,17 @@
+public class majority_element {
+    public int majorityElement(int[] nums) {
+        int res = 0, c = 0;
+        for(int i: nums) {
+            if(c == 0) {
+                res = i;
+            }
+            if(res == i) {
+                c++;
+            }
+            else {
+                c--;
+            }
+        }
+        return res;
+    }
+}
