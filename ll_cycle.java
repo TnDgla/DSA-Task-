0c10cd58393297826a27e9d8a4eefd164aa9a830
suryasingh999 <suryasingh2116@gmail.com>
2024-10-22 14:05:06 +0530

    Add files via upload

---
+++ ll_cycle.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class ll_cycle {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null) {
+            if(set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
---
+++ max_consecutive_ones.java
@@ -0,0 +1,15 @@
+public class max_consecutive_ones {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int res = 0, c = 0;
+        for(int i: nums) {
+            if(i == 1) {
+                c++;
+                res = Math.max(res, c);
+            }
+            else {
+                c = 0;
+            }
+        }
+        return res;
+    }
+}
