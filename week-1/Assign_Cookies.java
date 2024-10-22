e269109a577fed991224ef0c0fed95cbeefa47bf
Kavi-Singh <116540006+Kavi-Singh@users.noreply.github.com>
2024-10-22 14:59:08 +0530

    Add files via upload

---
+++ week-1/Assign_Cookies.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+
+            }
+            else j++;
+        }
+       return count;
+    }
+}
