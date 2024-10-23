17e7518ce041a2a387eababbb5811f00b622ad2f
rohitsingh290320052 <rohit.singh2_cs22@gla.ac.in>
2024-10-23 17:33:49 +0530

    maximum product subarray

--- TaskDSA.java
+++ TaskDSA.java
@@ -328,3 +328,27 @@ class Longest_Mountain_arry{
     }
 
 }
+
+class maximum_product_subarray{
+    class Solution {
+        public int maxProduct(int[] nums) {
+            if (nums == null || nums.length == 0) return 0;
+            int maxProduct = nums[0];
+            int minProduct = nums[0];
+            int result = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                int current = nums[i];
+                if (current < 0) {
+                    int temp = maxProduct;
+                    maxProduct = minProduct;
+                    minProduct = temp;
+                }
+                maxProduct = Math.max(current, maxProduct * current);
+                minProduct = Math.min(current, minProduct * current);
+                result = Math.max(result, maxProduct);
+            }
+
+            return result;
+        }
+    }
+}
